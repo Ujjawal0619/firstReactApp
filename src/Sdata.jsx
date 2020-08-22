bd13c113db27ee6f8299e87eeb5db4910402d6d2
Ujjawal kumar <ujjawal.kumar0619@gmail.com>
2020-08-22 22:18:07 +0530

    Creating react  Component

---
+++ src/Sdata.jsx
@@ -0,0 +1,50 @@
+const Sdata = [
+  {
+    sname: "ALTERED CARBON",
+    imgsrc: "https://wallpapercave.com/wp/wp2369766.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80097140",
+  },
+  {
+    sname: "PROJECT POWER",
+    imgsrc: "https://wallpapercave.com/wp/wp7188881.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80204465",
+  },
+  {
+    sname: "SACRED GAMES",
+    imgsrc: "https://wallpapercave.com/wp/wp3814512.png",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80115328",
+  },
+  {
+    sname: "DELHI CRIME",
+    imgsrc: "https://wallpapercave.com/wp/wp1891549.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/81076756",
+  },
+  {
+    sname: "EXTRACTION",
+    imgsrc: "https://wallpapercave.com/wp/wp6157194.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80230399",
+  },
+  {
+    sname: "MIND HUNTER",
+    imgsrc: "https://wallpapercave.com/wp/wp2228911.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80114855",
+  },
+  {
+    sname: "BODYGYARD",
+    imgsrc: "https://wallpapercave.com/wp/wp6372214.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80235864",
+  },
+  {
+    sname: "LOST IN SPACE",
+    imgsrc: "https://wallpapercave.com/wp/wp4096778.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80104198",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,41 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(
-  <>
-    <div className="cards">
-      {/* card 1 */}
-      <div classNam="card">
-        <img
-          src="https://wallpapercave.com/wp/wp7188881.jpg"
-          alt="myPic"
-          className="card__img"
-        />
-        <div className="card__info">
-          <span className="card__category">A Netflix Original</span>
-          <h3 className="card__title">PROJECT POWER</h3>
-          <a href="https://www.netflix.com/in/title/80204465" target="black">
-            <button> Watch Now </button>
-          </a>
+function Cards() {
+  // creating component, not a function.
+  // can be used any number of times as HTML element.
+  return (
+    <>
+      <div className="cards">
+        {/* card 1 */}
+        <div classNam="card">
+          <img
+            src="https://wallpapercave.com/wp/wp7188881.jpg"
+            alt="myPic"
+            className="card__img"
+          />
+          <div className="card__info">
+            <span className="card__category">A Netflix Original</span>
+            <h3 className="card__title">PROJECT POWER</h3>
+            <a href="https://www.netflix.com/in/title/80204465" target="black">
+              <button> Watch Now </button>
+            </a>
+          </div>
         </div>
       </div>
+    </>
+  );
+}
 
-      {/* card 2 */}
-      <div classNam="card">
-        <img
-          src="https://wallpapercave.com/wp/wp3814512.png"
-          alt="myPic"
-          className="card__img"
-        />
-        <div className="card__info">
-          <span className="card__category">A Netflix Original</span>
-          <h3 className="card__title">SECRED GAMES</h3>
-          <a href="https://www.netflix.com/in/title/80115328" target="black">
-            <button> Watch Now </button>
-          </a>
-        </div>
-      </div>
-    </div>
-  </>,
-  document.getElementById("root")
+// what to show?, where to show?, callback func.
+ReactDOM.render(
+  <Cards />,
+  document.getElementById("root") // where to show?
 );
