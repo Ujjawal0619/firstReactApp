ff339cd02e29313d4e371cab74eddd70373b79be
Ujjawal kumar <ujjawal.kumar0619@gmail.com>
2020-08-25 21:51:06 +0530

    final standard structure of the project:

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-// using using fat arrow function with map in previous version of file.
-
+// Standard Structure of React App
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import Card from "./Cards";
-import Sdata from "./Sdata";
+import App from "./App";
 
-ReactDOM.render(
-  <>
-    <h1 className="heading">My Top Favorite Netflix Web Series</h1>
-    <div className="container">
-      {Sdata.map((curVal) => (
-        <Card sname={curVal.sname} imgsrc={curVal.imgsrc} link={curVal.link} />
-      ))}
-    </div>
-  </>,
-  document.getElementById("root")
-);
-
-//look how compact this project become from first HTML/CSS version to now.
+ReactDOM.render(<App />, document.getElementById("root"));
